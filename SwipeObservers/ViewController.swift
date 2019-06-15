ffa4832387e333d35220db506b0974685871be21
id0000512 <id0000512@gmail.com>
2019-06-15 18:08:07 +0600

    01 Swipe direction in label

--- SwipeObservers/ViewController.swift
+++ SwipeObservers/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func swipesObservers() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    @objc func handleSwipes(gester: UISwipeGestureRecognizer) {
+        switch gester.direction {
+        case .right:
+            label.text = "Right swipe"
+        case .left:
+            label.text = "Left swipe"
+        case .up:
+            label.text = "Up swipe"
+        case .down:
+            label.text = "Left swipe"
+        default:
+            break
+        }
+    }
 
 }
 
