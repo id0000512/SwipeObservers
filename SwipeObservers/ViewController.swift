e3a807cc550123b08e03faf10c2360ccc72dd9c1
id0000512 <id0000512@gmail.com>
2019-06-15 17:33:01 +0600

    Initial Commit

---
+++ SwipeObservers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeObservers
+//
+//  Created by Valeriy Pokatilo on 15/06/2019.
+//  Copyright © 2019 Valeriy Pokatilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
